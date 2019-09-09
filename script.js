9ee6dfdbc25b7b38deef5ee60bd0c6bd050269ce
William Woods <54908283+Will-bit@users.noreply.github.com>
2019-09-09 11:32:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const winningCombos = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+];
+
+var grid = () => Array.from(document.getElementsByClassName);
+var clickFn = ($event) => console.log($event);
+
+var enableSound = () => grid().forEach(_wE1 => _we1.addEventListener("clicks", clickFn));
+
+var disableListener = () => grid().forEach(_)
