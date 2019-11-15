6205edbcbe6c2dee5a709d6cf0b7278fcb977b88
William Woods <54908283+Will-bit@users.noreply.github.com>
2019-11-14 20:59:01 -0600

    Update script.js
    forgot to update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const winningCombos = [
+/*const winningCombos = [
   [0,1,2],
   [3,4,5],
   [6,7,8],
@@ -7,10 +7,86 @@ const winningCombos = [
   [1,4,7],
   [2,5,8],
 ];
+*/
+var game = document.querySelectorAll(".w")  // W is name of table, linking HTML cellIndex
+var currentPlayer = "X"
+/// Win column for game
+let winningCom =[
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+// Letting me select everything in my table, all tds
+game.forEach(function(square){
+  square.addEventListener('click', spotClicked);
+});
+let xClicks=[];
+let oClicks = [];
 
-var grid = () => Array.from(document.getElementsByClassName);
-var clickFn = ($event) => console.log($event);
+ 
 
-var enableSound = () => grid().forEach(_wE1 => _we1.addEventListener("clicks", clickFn));
+/// Jordan helped; Alter between X and O.  Current player set for X
+function spotClicked(){
+  game.innerHTML=currentPlayer;
+  if(currentPlayer === "X"){
+    event.target.innerHTML="X";
+    xClicks.push(game.id);
+    checkForWinner();
+    currentPlayer = "O";
+   
+  } else if (currentPlayer == 'O'){
+    event.target.innerHTML ="O";
+    oClicks.push(game.id);
+    checkForWinner();
+    currentPlayer = "X";
+    
+  }
+}
 
-var disableListener = () => grid().forEach(_)
+// Check for winner via array
+function checkForWinner() {
+  winningCom.forEach(function(combo) {
+    let winArray = [];
+
+
+    combo.forEach(function(number){
+      if(xClicks.join().includes(number)){
+        winArray.push('X')
+      }   else if(oClicks.join().includes(number)){
+        winArray.push('O')
+      }
+    });
+
+    setTimeout(function(){
+  if(winArray.join() == 'X,X,X') {
+    alert('X wins');
+    clear();
+  } else if(winArray.join() == "O,O,O") {
+  alert("O Wins");
+  clear();
+ }
+}, 50);
+  })
+}
+
+
+    function clear(){
+      xClicks = [];
+      oClicks = [];
+
+      currentPlayer = "X";
+    
+      
+      for(let square of game){
+      square.innerHTML= '';
+    }
+    }
+
+/* square.forEach(function{
+game.innerHTML */
+
